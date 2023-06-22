ca9eb9dc7a7fc0426161e5712c097b0b214e88e6
Andy Wu <awpats12@gmail.com>
2023-06-21 22:57:17 -0400

    Fetch user's tasks client-side

--- client/src/routes/Dashboard/TaskList.tsx
+++ client/src/routes/Dashboard/TaskList.tsx
@@ -1,8 +1,43 @@
+import { useState, useEffect } from "react";
 
 export default function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const username = JSON.parse(sessionStorage.getItem('user')!).username;
+
+    const queryBody = `query {
+      tasksForUser(username: "${username}") {
+        id
+        title
+        description
+        status
+        dueDate
+        creatorId
+      }
+    }`
+
+    /// TODO: Move to custom hook
+    const fetchUserTasks = async () => {
+      const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/graphql`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          'Content-Type': 'application/graphql'
+        },
+        body: queryBody
+      });
+
+      const json = await response.json();
+      setTasks(json.tasksForUser)
+    }
+
+    fetchUserTasks();
+  }, []);
+
   return (
     <>
-    
+
     </>
   )
 }
