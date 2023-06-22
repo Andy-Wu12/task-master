a2c516502b00c61c75abe8782705fb328476b97d
Andy Wu <awpats12@gmail.com>
2023-06-22 01:31:59 -0400

    User's tasks now displayed

--- client/src/routes/Dashboard/TaskList.tsx
+++ client/src/routes/Dashboard/TaskList.tsx
@@ -1,44 +1,44 @@
-import { useState, useEffect } from "react";
+import useTasks, { TaskStatus } from "../../hooks/useTasks"
+
+import type { Task } from '../../hooks/useTasks';
+
+import Checkbox from 'react-bootstrap/FormCheck';
 
 export default function TaskList() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    const username = JSON.parse(sessionStorage.getItem('user')!).username;
-
-    const queryBody = `query {
-      tasksForUser(username: "${username}") {
-        id
-        title
-        description
-        status
-        dueDate
-        creatorId
-      }
-    }`
-
-    /// TODO: Move to custom hook
-    const fetchUserTasks = async () => {
-      const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/graphql`, {
-        method: "POST",
-        credentials: "include",
-        headers: {
-          'Content-Type': 'application/graphql'
-        },
-        body: queryBody
-      });
-
-      const json = await response.json();
-      setTasks(json.tasksForUser)
-    }
-
-    fetchUserTasks();
-  }, []);
+  const {
+    userTasks
+  } = useTasks();
 
   return (
     <>
-
+      {userTasks.map((task: Task) => {
+        return <ol key={task.id}> <TaskListItem task={task} /> <br/> </ol>
+      })}
     </>
   )
 }
 
+interface TaskListItemProps {
+  task: Task
+}
+
+function TaskListItem({task}: TaskListItemProps) {
+  const {
+    id,
+    title,
+    description,
+    status,
+    dueDate,
+    creatorId
+  } = task;
+
+  return (
+    <>
+      <div className='task-list-item'>
+        <h2>{title}</h2>
+        <h3>{description}</h3>
+        <Checkbox label={status} checked={status === TaskStatus.COMPLETED} readOnly/>
+      </div>
+    </>
+  )
+}
