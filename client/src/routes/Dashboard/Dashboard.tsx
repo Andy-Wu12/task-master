911a2a3efe1b37ee0e2c73b8e9a5b5adece2fc96
Andy Wu <awpats12@gmail.com>
2023-06-23 01:47:47 -0400

    Added logout functionality

--- client/src/routes/Dashboard/Dashboard.tsx
+++ client/src/routes/Dashboard/Dashboard.tsx
@@ -1,14 +1,27 @@
 import { Outlet } from "react-router-dom";
+import { useContext } from "react";
+
+import Button from "react-bootstrap/Button";
 
 import Navbar from "../../components/Nav/Nav";
+import { AuthContext } from "../../context/authContext";
 
 export default function Dashboard() {
+  const authContext = useContext(AuthContext);
+
+  const logout = () => {
+    authContext.setUser(null);
+    window.sessionStorage.removeItem('user');
+  }
+
   return (
     <>
       <h2>Dashboard</h2>
       <Navbar /> <br/>
       
-      <Outlet />
+      <Outlet /> <br/>
+
+      <Button onClick={logout}>Logout</Button>
     </>
   );
 }
